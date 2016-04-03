c9c69cfdd9a6718ad86f9427b872a4d6751d33bf
Han Hwu <hanhwu@yahoo.com>
2016-04-02 22:53:50 -0700

    Better algorithm for py003

--- py003.py
+++ py003.py
@@ -2,57 +2,19 @@
 ##
 ## What is the largest prime factor of the number 600851475143 ?
 
-## First solution ran out of memory
-##
-##import math
-##
-##def makeprimelist(upperlimit):
-##    """Input int upperlimit; output list of primes up to upperlimit"""
-##    primelist = range(2,upperlimit,1)
-##    i = 0
-##    sifter = primelist[i]
-##    while sifter < int(math.sqrt(max(primelist)))+1:
-##        j = i+1
-##        while j < len(primelist):
-##            if primelist[j]%sifter == 0:
-##                del primelist[j]
-##            else:
-##                j += 1
-##        i += 1
-##        sifter = primelist[i]
-##        print sifter
-##    return primelist
-##
+def FindLargestPrimeFactor(x):
+    x = 600851475143
+    ans = 1
+    i = 2
+    while i <= x:
+        if x%i == 0:
+            x = x / i
+            ans = i
+            ## progress checkprint: print 'x=',x,' ans=',ans
+            i = 2
+        i += 1
 
-
-## Second attempt
-## mybranch
-
-import math
-
-def makeprimelist(upperlimit):
-    """Input int upperlimit; output list of primes up to upperlimit"""
-    primelist = [2,3]
-    x = 5
-    while x < upperlimit:
-        addtolist = True
-        for i in primelist:
-            if x%i == 0:
-                addtolist = False
-                break
-        if addtolist:
-            primelist.append(x)
-            print x
-        x += 1
-    return primelist
-
-x = 600851475143
-ans = 1
-primestocheck = makeprimelist(x)
-for i in primestocheck:
-    if x%i == 0:
-        ans = i
-print 'ans=',ans
+print FindLargestPrimeFactor(600851475143)
 
                 
             
