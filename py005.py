8fcc44b58c9daa42f5ddbbdcf85e567613a857e2
Han Hwu <hanhwu@yahoo.com>
2016-04-04 09:37:09 -0700

    YAPF py005.py

--- py005.py
+++ py005.py
@@ -5,40 +5,43 @@
 ## What is the smallest positive number that is evenly
 ## divisible by all of the numbers from 1 to 20?
 
+
 # Function to find smallest positive number that is evenly
 # divisible by numbers from a to b
-def small_mult(a,b):
+def small_mult(a, b):
     """in: int a,b; out: int"""
-    divisors = range(a,b+1)
+    divisors = range(a, b + 1)
     common_primes = []
     while len(divisors) > 0:
         # check for even divisors from 2 and iterate up
         # when found, update divisors, restart from 2, and add to common_primes list
         common_found = False
-        for j in range(2,max(divisors)+1):
+        for j in range(2, max(divisors) + 1):
             for i in range(len(divisors)):
-                if divisors[i]%j == 0:
-                    divisors[i] = divisors[i]/j
+                if divisors[i] % j == 0:
+                    divisors[i] = divisors[i] / j
                     common_found = True
             if common_found:
                 common_primes.append(j)
                 break
         # remove 1s in divisors
-        i = len(divisors)-1
+        i = len(divisors) - 1
         while i >= 0:
             if divisors[i] == 1:
                 del divisors[i]
             i -= 1
-            
+
     result = 1
     for i in common_primes:
-        result = result*i
+        result = result * i
     return result
-        
-# Problem setup (1,10 -> 2520 verified)
-# Inefficient method found 1,20 -> 232792560
+
+    # Problem setup (1,10 -> 2520 verified)
+    # Inefficient method found 1,20 -> 232792560
+
+
 x = 1
 y = 20
 print 'Running!'
-print 'Ans = ',small_mult(x,y)
+print 'Ans = ', small_mult(x, y)
 print 'Done!'
