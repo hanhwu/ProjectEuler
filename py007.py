7b4e3b3c220b7258aae6cd8ab3f72a09764cd97d
Han Hwu <hanhwu@yahoo.com>
2016-04-04 11:33:55 -0700

    py007 architecture established

---
+++ py007.py
@@ -0,0 +1,19 @@
+# By listing the first six prime numbers:
+# 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# 
+# What is the 10001st prime number?
+
+def find_nth_prime(n):
+    """input: n-th prime to find (int)
+    output: n-th prime (int)"""
+    primes = [2]
+
+    while len(primes) < n
+        # keep adding primes to the end of the primes list
+        # until desired n is reached
+
+    return primes[-1]
+
+
+print find_nth_prime(6)
+#print find_nth_prime(10001)
