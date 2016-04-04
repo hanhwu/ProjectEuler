7401b2fb39ec6a45ad8b02a9437e7696ad948d45
Han Hwu <hanhwu@yahoo.com>
2016-04-04 00:23:12 -0700

    branching py005 to find a faster method

--- py005.py
+++ py005.py
@@ -6,20 +6,37 @@
 ## divisible by all of the numbers from 1 to 20?
 
 # Function to find smallest positive number that is evenly
-# divisible by numbers from x to y
+# divisible by numbers from a to b
 def small_mult(a,b):
     """in: int a,b; out: int"""
-    ans = 1
-    solution_unknown = True
-    while solution_unknown:
-        for i in range(a,b+1):
-            if ans%i != 0:
-                ans += 1
-                break
-            elif i == b:
-                solution_unknown = False
-                break
-    return ans
+##    ans = 1
+##    solution_unknown = True
+##    while solution_unknown:
+##        for i in range(a,b+1):
+##            if ans%i != 0:
+##                ans += 1
+##                break
+##            elif i == b:
+##                solution_unknown = False
+##                break
+##    return ans
+
+    divisors = range(a,b+1)
+    common_primes = []
+    while len(divisors) > 0:
+# remove 1s in divisors
+        i = len(divisors)-1
+        while i >= 0:
+            if divisors[i] == 0:
+                del divisors[i]
+            i -= 1
+# check for divisors mod 2 and iterate up
+# when found, divide, replace, restart, and add to common_primes list
+        j = 2
+            
+            
+            
+            
 
 
 # Problem setup (1,10 -> 2520 verified)
