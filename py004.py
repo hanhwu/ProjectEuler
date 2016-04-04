128c6f5e24d4d6514c424339e0b83c9299a9215e
hanhwu <hanhwu@yahoo.com>
2016-04-03 21:49:17 -0700

    solution found

--- py004.py
+++ py004.py
@@ -5,15 +5,22 @@
 ## Find the largest palindrome made from the product of
 ## two 3-digit numbers.
 
-def ispalindrome(num):
-    ## checks if a number is a palindrome
-    return verdict
+def ispalindrome(numstr):
+    """input str; returns bool"""
+    if len(numstr)>2 and numstr[0]==numstr[-1]:
+        return ispalindrome(numstr[1:-1])
+    elif len(numstr) == 2 and numstr[0]==numstr[1]:
+        return True
+    elif len(numstr) == 1:
+        return True
+    else:
+        return False
 
-
+## setup for problem 004
+    
 ans = 0
 for i in range(100,1000,1):
     for j in range (i+1,1000,1):
-        if ispalindrome(i*j) and i*j > ans:
+        if ispalindrome(str(i*j)) and i*j > ans:
             ans = i*j
-
 print ans
