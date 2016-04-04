199a72ec5ef089d54acde79b4cb9a3c52ce7cf18
Han Hwu <hanhwu@yahoo.com>
2016-04-04 10:05:31 -0700

    YAPF py006.py

--- py006.py
+++ py006.py
@@ -10,11 +10,13 @@
 ## Find the difference between the sum of the squares of the
 ## first one hundred natural numbers and the square of the sum.
 
+
 def sum_sq_diff(a):
     """input a (int); output sum-sq-diff of first 'a' natural numbers (int)"""
-    ans = sum(range(a+1))**2
-    for i in range(1,a+1):
+    ans = sum(range(a + 1))**2
+    for i in range(1, a + 1):
         ans = ans - i**2
     return ans
+
 
 print sum_sq_diff(100)
