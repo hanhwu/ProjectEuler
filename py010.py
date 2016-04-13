6fcc897fe975f947b367b111820d1d933a1e0b9b
Han Hwu <hanhwu@yahoo.com>
2016-04-13 09:26:58 -0700

    py010 setup and draft untested solution

---
+++ py010.py
@@ -0,0 +1,33 @@
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+# Find the sum of all the primes below two million.
+
+import math
+import time
+
+def find_prime(n):
+    """input: n as search limit
+    output: list of primes"""
+    primes = [2]
+    guess = 3
+    while primes[-1] < n:
+        # keep adding primes to the end of the primes list
+        # until desired n is reached
+        isprime = True
+        for i in primes:
+            if guess % i == 0:
+                isprime = False
+                break
+            elif i > math.sqrt(guess):
+                break
+        if isprime:
+            primes.append(guess)
+        guess += 1
+
+    return primes[0:-2]  # because last one exceeds n
+
+n = 2000000
+start = time.time()
+plist = find_primes(n)
+solve_time = time.time() - start
+ans = sum(plist)
+print 'Answer',ans,'found in',solve_time,'seconds.'
