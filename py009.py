5c970ed9212cb9c9d734253068942bdba0ad42a9
Han Hwu <hanhwu@yahoo.com>
2016-04-13 09:04:38 -0700

    Untested draft solution of py009.py

--- py009.py
+++ py009.py
@@ -1,7 +1,30 @@
-A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#
+# a^2 + b^2 = c^2
+# For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+#
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
 
-a^2 + b2 = c2
-For example, 32 + 42 = 9 + 16 = 25 = 52.
+import time
 
-There exists exactly one Pythagorean triplet for which a + b + c = 1000.
-Find the product abc.
+def find_triplet(n):
+  """Returns A*B*C for A**2 + B**2 = C**2, and A+B+C=n"""
+  ## range of guesses:
+  ## a:    1    to  n//3
+  ## b:    a+1  to  a+1 + (n-a)//2
+  ## c:    n-a-b
+  ## check that a<b and b<c, there are edge cases
+  ans = None
+  for a in range(1, n // 3 + 1):
+    for b in range(a + 1, a + 1 + (n - a) // 2 + 1):
+      c = n - a - b
+      if a < b and b < c and a**2 + b**2 == c**2:
+        ans = a * b * c
+  return ans
+  
+start = time.time()
+n = 1000
+ans = find_triplet(n)
+solve_time = time.time() - start
+print 'Answer',ans,'found in',solve_time,'seconds'
