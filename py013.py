fef6585917032485aa8d337f63bf9c1cc130d9c8
Han Hwu <hanhwu@yahoo.com>
2016-04-25 14:07:30 -0700

    drafted untested code for solving 013

---
+++ py013.py
@@ -0,0 +1,23 @@
+# Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+
+import time
+
+start = time.time()
+
+f = open('013ref.txt','r')
+line = f.readline()
+largenumbersum = 0
+
+while line != '':
+	numstring = ''
+	for i in line:
+		if i != '\n':
+			numstring += i
+			
+	largenumbersum += int(numstring)
+	line = f.raedline()
+	
+ans = str(largenumbersum)[range(10)]
+dur = time.time() - start
+print 'Answer', ans, 'found in', dur, 'seconds.'
+
