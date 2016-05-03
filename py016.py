a74b8b882778ce4745ce60b36d4110171bc8fee3
Han Hwu <hanhwu@yahoo.com>
2016-05-03 11:19:41 -0700

    [Untested] Setup and drafted solution

---
+++ py016.py
@@ -0,0 +1,18 @@
+# Problem 16:  Power Digit Sum
+#	What is the sum of the digits of 2**1000?
+
+import time
+
+def solve16():
+	start = time.time()
+	x = 2**1000
+	ans = 0
+	for c in str(x):
+		ans += int(c)
+	
+	dur = time.time() - start
+	print 'Answer', ans, 'found in', dur, 'seconds.'
+	
+
+solve16()
+	
