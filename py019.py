4f24e7fddab990d65d2b35cf7818bcce63565f0a
Han Hwu <hanhwu@yahoo.com>
2016-05-12 15:49:25 -0700

    Setup problem 19

---
+++ py019.py
@@ -0,0 +1,8 @@
+# Counting Sundays
+# 1900/1/1 was a Monday
+# How many Sundays fell on the first of the month in the 20th Century? (1901/1/1 to 2000/12/31)
+# Leap year conditions:
+#   Year divisible by 4
+#   Year divisible by 400
+#   NOT Year divisible by 100, but not 400 
+#   (i.e. 2000 leaps, 1900 doesn't)
