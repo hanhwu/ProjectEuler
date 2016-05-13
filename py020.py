54c739390d5a723f00c691d081c84e4e89405d16
Han Hwu <hanhwu@yahoo.com>
2016-05-13 10:36:14 -0700

    [Untested] Setup and drafted solution

---
+++ py020.py
@@ -0,0 +1,29 @@
+# Factorial Digit Sum
+# Find digit sum of 100!
+
+import time
+
+def factorial(n):
+    assert type(n) is int
+    if n == 0:
+        return 1
+        
+    else:
+        return n * factorial(n - 1)
+
+
+def sumdigits(n):
+    assert type(n) is int
+    cn = 0
+    for c in str(n):
+        cn += int(c)
+        
+    return cn
+
+
+def solve020():
+    ans = sumdigits(factorial(100))
+    print 'Answer', ans, 'found.'
+
+
+solve020()
