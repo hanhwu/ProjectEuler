8b68eeb9391acd945c1bf356126a5f38906fd040
Han Hwu <hanhwu@yahoo.com>
2016-05-13 10:06:29 -0700

    [Untested] drafted solution

--- py019.py
+++ py019.py
@@ -6,3 +6,23 @@
 #   Year divisible by 400
 #   NOT Year divisible by 100, but not 400 
 #   (i.e. 2000 leaps, 1900 doesn't)
+
+from datetime import datetime, date, timedelta
+
+def solve019():
+    startdate = date(1901, 1, 1)
+    enddate = date(2000, 12, 31)
+    testdate = startdate
+    inc = timedelta(days = 1)
+    ans = 0
+    while testdate <= enddate:
+        if testdate.day() == 1 and testdate.weekday() == 6:
+            ans += 1
+        
+        testdate += inc
+    
+    
+    print "Answer", ans, "found."
+
+
+solve019()
