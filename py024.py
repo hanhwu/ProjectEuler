d02c60c1efe08d3e631a7f8771510e685789cdd8
Han Hwu <hanhwu@yahoo.com>
2016-05-17 10:52:56 -0700

    partial solution drafted

--- py024.py
+++ py024.py
@@ -7,3 +7,23 @@
 # 012   021   102   120   201   210
 # What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
+import time
+
+def permutation_gen(already_done, add_these):
+    for c in add_these:
+        # continue here
+    pass
+
+
+def solve024():
+    start = time.time()
+    permu_members_int = range(10)
+    permu_members_str = []
+    for i in permu_members_int:
+        permu_members_str.append(str(i))
+        
+    permu_results = permutation_gen('', permu_members_str)
+    pass
+
+
+solve024()
